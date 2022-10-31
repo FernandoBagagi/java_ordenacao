1d045eaed95ad5b4103ef1cb3e13e2b4f331d2d7
Fernando <fernandosmartphone96@gmail.com>
2022-10-31 09:57:25 -0300

    cria lista interna no Bubble Sort

--- src/ordenacao/BubbleSort.java
+++ src/ordenacao/BubbleSort.java
@@ -8,17 +8,18 @@ public class BubbleSort<T> implements Ordenador {
 
     @Override
     public List<Integer> ordena(List<Integer> lista) {
+        lista = new LinkedList<>(lista);
         for (int i = 0; i < lista.size() - 1; i++) {
             for (int j = 0; j < lista.size() - 1; j++) {
                 if (lista.get(j) > lista.get(j + 1)) {
                     Integer aux = lista.get(j);
                     lista.set(j, lista.get(j + 1));
                     lista.set(j + 1, aux);
-                    System.out.println(lista);
+                    //System.out.println(lista);
                 }
             }
         }
-        return lista;
+        return new ArrayList<>(lista);
     }
 
 }
