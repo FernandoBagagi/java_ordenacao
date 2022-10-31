3949c534da8a6bdd5c81b8e56f0e84ea1f93938e
Fernando <fernandosmartphone96@gmail.com>
2022-10-31 10:21:07 -0300

    cria estrutura para avaliação dos resultado

--- src/App.java
+++ src/App.java
@@ -6,6 +6,15 @@ import ordenacao.Ordenador;
 
 public class App {
     public static void main(String[] args) {
+
+        /*
+         int quantidade = 10000;
+        int[] vetor = new int[quantidade];
+
+        for (int i = 0; i < vetor.length; i++) {
+         vetor[i] = (int) (Math.random()*quantidade);
+        }
+         */
         List<Integer> numeros = new ArrayList<>();
         numeros.add(10);
         numeros.add(65);
@@ -19,7 +28,27 @@ public class App {
         numeros.add(302);
 
         System.out.println("Antes da ordenação: " + numeros);
+        long tempoInicial = System.currentTimeMillis();
         Ordenador ordenador = new BubbleSort<>();
-        System.out.println("Após a ordenação:   " + ordenador.ordena(numeros));
+        long tempoFinal = System.currentTimeMillis();
+        System.out.println("Após a ordenação:   " + ordenador.ordena(numeros) + "  " + (tempoFinal - tempoInicial) + " ms");
+
+        /**
+         * Sites:
+         * -> https://www.freecodecamp.org/portuguese/news/algoritmos-de-ordenacao-explicados-com-exemplos-em-python-java-e-c/
+         * -> https://www.devmedia.com.br/algoritmos-de-ordenacao-em-java/32693
+         * 
+         * Ordenações:
+         * 
+         * -> Bubble Sort
+         * -> Insertion Sort
+         * -> Selection Sort
+         * -> Merge Sort
+         * -> Quick Sort
+         * -> Heap Sort
+         * -> Counting Sort
+         * -> Radix Sort
+         * -> Bucket Sort
+         */
     }
 }
