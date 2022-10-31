8ef349937a1dcaa54d3343e7c8e03b860cebbf49
Fernando <fernandosmartphone96@gmail.com>
2022-10-31 09:54:17 -0300

    testa o funcionamento do Bubble Sort

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import ordenacao.BubbleSort;
+import ordenacao.Ordenador;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(10);
+        numeros.add(65);
+        numeros.add(47);
+        numeros.add(35);
+        numeros.add(7412);
+        numeros.add(0);
+        numeros.add(98);
+        numeros.add(73);
+        numeros.add(24);
+        numeros.add(302);
+
+        System.out.println("Antes da ordenação: " + numeros);
+        Ordenador ordenador = new BubbleSort<>();
+        System.out.println("Após a ordenação: " + ordenador.ordena(numeros));
     }
 }
