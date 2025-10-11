2071ec76fb529ca7a8dfe09e1fc1b86c37277f26
Fernando Bagagi <fernandosmartphone96@gmail.com>
2025-10-11 16:29:33 -0300

    testa GeradorLista na main

--- src/main/java/br/com/ferdbgg/App.java
+++ src/main/java/br/com/ferdbgg/App.java
@@ -1,10 +1,29 @@
 package br.com.ferdbgg;
 
+import java.util.random.RandomGenerator;
+
 public class App {
     public static void main(String[] args) {
-        //TODO: Criar listas de testes
-        //TODO: Criar ordenadores que serão testados
-        //TODO: Executar os testes
-        //TODO: Gerar relatótio
+
+        final GeradorObjeto<Integer> geradorInteirosAleatorios = new GeradorObjeto<Integer>() {
+            
+            final RandomGenerator randomGenerator = RandomGenerator.getDefault();
+            
+            @Override
+            public Integer gerarObjeto() {
+                return randomGenerator.nextInt(0, Integer.MAX_VALUE);
+            }
+
+        };
+
+        final GeradorLista<Integer> geradorLista = new GeradorLista<>(geradorInteirosAleatorios);
+        geradorLista.gerarNovaLista(10, false, 100);
+
+        System.out.println(geradorLista.getLista());
+
+        // TODO: Criar listas de testes
+        // TODO: Criar ordenadores que serão testados
+        // TODO: Executar os testes
+        // TODO: Gerar relatótio
     }
 }
